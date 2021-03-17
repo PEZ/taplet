30f0eaf0b265f42ba2edf903ce864a9a199468fa
Peter Strömberg <pez@pezius.com>
2021-03-17 01:00:39 +0100

    Add some tests

--- src/pez/taplet.cljc
+++ src/pez/taplet.cljc
@@ -2,14 +2,27 @@
   #?(:cljs
      (:require-macros [pez.taplet :refer [let>]])))
 
-(defmacro let>
-  "Like `let`, plus `tap>`s the binding box"
-  [bindings & body]
+(defmacro let>l
+  "Like `let>`, adding a label first in the tapped vector"
+  [label bindings & body]
+  (assert (or (nil? label)
+              (keyword? label))
+          "`label` is not a keyword")
   (let [to-vec (fn [bindings]
-                 (into []
-                       (mapcat (fn [b-name]
-                                 [(if (symbol? b-name) (keyword b-name) :-destruct-) b-name])
-                               (flatten (partition 1 2 bindings)))))]
+                 (into (if label [label] [])
+                       (mapcat (fn [bind-to]
+                               [(if (symbol? bind-to)
+                                  (keyword bind-to)
+                                  (pr-str bind-to))
+                                bind-to])
+                             (flatten (partition 1 2 bindings)))))]
     `(let ~(destructure bindings)
        (tap> ~(to-vec bindings))
        ~@body)))
+
+(defmacro let>
+  "Like `let`, plus `tap>`s the binding box"
+  [bindings & body]
+  (assert (vector? bindings)
+          "`bindings` is not a vector")
+  `(let>l nil ~bindings ~@body))
--- test/pez/taplet_test.clj
+++ test/pez/taplet_test.clj
@@ -1,7 +1,81 @@
 (ns pez.taplet-test
-  (:require [clojure.test :refer :all]
-            [pez.taplet :refer :all]))
+  (:require [clojure.test :refer [deftest testing is]]
+            [pez.taplet :as sut]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+
+(deftest let>
+  (testing "Evaluates as `let`"
+    (is (= [:foo :bar] (sut/let> [foo :foo
+                                  bar :bar]
+                                 [foo bar]))))
+
+  (testing "Taps the binding box"
+    (let [tapped (atom nil)
+          save-tap (fn [v] (reset! tapped v))]
+      (add-tap save-tap)
+      (is (= [:foo :bar]
+             (sut/let> [foo :foo
+                        bar :bar]
+                       [foo bar])))
+      (is (= [:foo :foo
+              :bar :bar]
+             @tapped))
+      (remove-tap save-tap)))
+
+  (testing "Taps vector destructuring bind-to flattened"
+    (let [tapped (atom nil)
+          save-tap (fn [v] (reset! tapped v))]
+      (add-tap save-tap)
+      (is (= [:foo :bar :baz :gaz]
+             (sut/let> [[a [b [c d]]] [:foo [:bar [:baz :gaz]]]]
+                       [a b c d])))
+      (is (= [:a :foo
+              :b :bar
+              :c :baz
+              :d :gaz]
+             @tapped))
+      (remove-tap save-tap)))
+
+  (testing "Taps map destructuring bind-to as a string"
+    (let [tapped (atom nil)
+          save-tap (fn [v] (reset! tapped v))]
+      (add-tap save-tap)
+      (is (= [2 {:x 2}]
+             (sut/let> [{:keys [x] :as y} {:x 2}]
+                       [x y])))
+      (is (= ["{:keys [x], :as y}" {:keys [2] :as {:x 2}}]
+             @tapped))
+      (remove-tap save-tap)))
+
+  (testing "Taps all the things as expected"
+    (let [tapped (atom nil)
+          save-tap (fn [v] (reset! tapped v))]
+      (add-tap save-tap)
+      (is (= [1 2 {:z 2} :foo :bar :baz :gaz]
+           (sut/let> [x 1
+                      {:keys [z] :as y} {:z 2}
+                      [a [b {:keys [c d]}]] [:foo [:bar {:c :baz :d :gaz}]]]
+                     [x z y a b c d])))
+      (is (= [:x 1
+              "{:keys [z], :as y}" {:keys [2] :as {:z 2}}
+              :a :foo
+              :b :bar
+              "{:keys [c d]}" {:keys [:baz :gaz]}]
+             @tapped))
+      (remove-tap save-tap))))
+
+(deftest let>l
+  (testing "Taps the binding box with the label first"
+    (let [tapped (atom nil)
+          save-tap (fn [v] (reset! tapped v))]
+      (add-tap save-tap)
+      (is (= [:foo :bar]
+             (sut/let>l :label
+                        [foo :foo
+                         bar :bar]
+                        [foo bar])))
+      (is (= [:label
+              :foo :foo
+              :bar :bar]
+             @tapped))
+      (remove-tap save-tap))))
